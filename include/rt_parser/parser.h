dc3470c2ea20cd3cfead2e15d177880a90a4b30d
j <castro.jaime@proton.me>
2026-03-05 23:02:30 +0100

    added: parse color signatures

--- include/rt_parser/parser.h
+++ include/rt_parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 00:33:19 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/03/05 23:01:06 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/03/05 23:02:27 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,19 @@ typedef struct s_color_result
 	t_u32a			color;
 }	t_color_result;
 
+typedef struct s_coord_result
+{
+	t_taggedresult	res;
+	t_3dcoords		color;
+}	t_coord_result;
+
 t_u32a			rt_parse_file_into_state(t_RTScene *state, char *fname)\
 					__attribute__((__nonnull__(1, 2)));
 
 t_color_result	rt_parse_color(t_tokenizer *tokenizer)\
 					__attribute__((__nonnull__(1)));
 
-t_color_result	rt_parse_coords(t_tokenizer *tokenizer)\
+t_coord_result	rt_parse_coords(t_tokenizer *tokenizer)\
 					__attribute__((__nonnull__(1)));
 
 #endif
