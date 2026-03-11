195a7f3ff5f96dac5cfea4fe52ae6baf08474bb0
j <castro.jaime@proton.me>
2026-03-11 02:10:53 +0100

    fix: use macro instead of constant

--- src/rt_parser/rt_init_aos.c
+++ src/rt_parser/rt_init_aos.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 17:24:20 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/03/08 01:15:36 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/03/11 02:10:40 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ inline t_taggedresult	rt_init_aos(t_vec aos[4])
 	aos[RT_AOS_LIGHT] = ft_vec_new(16, sizeof(t_RTLight));
 	if (!aos[RT_AOS_LIGHT].head)
 		return (ft_vec_free(aos), KO);
-	aos[2] = ft_vec_new(16, sizeof(t_RTPlane));
+	aos[RT_AOS_PLANE] = ft_vec_new(16, sizeof(t_RTPlane));
 	if (!aos[RT_AOS_PLANE].head)
 		return (ft_vec_free(aos), ft_vec_free(&aos[RT_AOS_LIGHT]), KO);
 	aos[RT_AOS_CYLINDER] = ft_vec_new(16, sizeof(t_RTCylinder));
