bc64b7ff34a2b45bf16a962661b688992019da61
j <castro.jaime@proton.me>
2026-03-13 16:43:33 +0100

    refactor: use actual X's keymasks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 18:08:13 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/03/13 16:39:00 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/03/13 16:43:23 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@ inline t_taggedresult	rt_mlx_setup(t_RTstate *state)
 			(char *)"miniRT");
 	if (!state->ctx.rt_mlx_win)
 		return (rt_error("Error\nmlx window\n"), KO);
-	mlx_hook(state->ctx.rt_mlx_win, 2, KeyPressMask, rt_key_press, state);
-	mlx_hook(state->ctx.rt_mlx_win, 3, KeyReleaseMask, rt_key_release, state);
-	mlx_hook(state->ctx.rt_mlx_win, 6, PointerMotionMask, rt_handle_mouse_move, state);
+	mlx_hook(state->ctx.rt_mlx_win, KeyPress, KeyPressMask, rt_key_press, state);
+	mlx_hook(state->ctx.rt_mlx_win, KeyRelease, KeyReleaseMask, rt_key_release, state);
+	mlx_hook(state->ctx.rt_mlx_win, MotionNotify, PointerMotionMask, rt_handle_mouse_move,
+		state);
 	mlx_loop_hook(state->ctx.rt_mlx, rt_render_hotloop, state);
 	return (OK);
 }
