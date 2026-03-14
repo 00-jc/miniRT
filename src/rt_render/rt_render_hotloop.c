b8d77497f462a7613f9a08dc781c9bbcd432f3ec
j <castro.jaime@proton.me>
2026-03-14 20:23:19 +0100

    remove fps counter + some other things i dont remember

--- src/rt_render/rt_render_hotloop.c
+++ src/rt_render/rt_render_hotloop.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 22:20:46 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/03/14 02:29:35 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/03/14 20:22:36 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,28 +57,6 @@ static inline t_taggedresult	rt_reload(t_RTstate *state)
 	return (OK);
 }
 
-__attribute__((__nonnull__(1), __always_inline__, hot))
-inline void	rt_printfps(t_RTstate *state)
-{
-	struct timeval	start;
-	struct timeval	end;
-	long			seconds;
-	long			useconds;
-	double			mtime;
-
-	gettimeofday(&start, NULL);
-	ft_arena_rewind(&state->ctx.rt_arena, state->ctx.rewind_render);
-	rt_render_frame(&state->ctx, &state->scene);
-	rt_putimg(&state->ctx);
-	gettimeofday(&end, NULL);
-	seconds = end.tv_sec - start.tv_sec;
-	useconds = end.tv_usec - start.tv_usec;
-	mtime = ((double)seconds * 1000.0)
-		+ ((double)useconds * 0.001);
-	if (mtime > 0)
-		ft_printf("fps: %f | frame time: %fms\r", 1000.0 / mtime, mtime);
-}
-
 __attribute__((__nonnull__(1), hot))
 int	rt_render_hotloop(t_RTstate *state)
 {
@@ -87,6 +65,10 @@ int	rt_render_hotloop(t_RTstate *state)
 		(rt_free_state(state), exit(EXIT_FAILURE));
 	rt_init_vp(state);
 	if (state->ctx.scene_redraw)
-		rt_printfps(state);
+	{
+		ft_arena_rewind(&state->ctx.rt_arena, state->ctx.rewind_render);
+		rt_render_frame(&state->ctx, &state->scene);
+		rt_putimg(&state->ctx);
+	}
 	return ((int)(state->ctx.scene_redraw = 0));
 }
