45caf7839965614cef1acba3d941052dd4f6d5d9
j <castro.jaime@proton.me>
2026-03-14 19:40:43 +0100

    added: packet tracer scaffolding

--- src/rt_render/rt_packet_tracer/rt_render_packet.c
+++ src/rt_render/rt_packet_tracer/rt_render_packet.c
@@ -6,7 +6,7 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 16:08:09 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/03/14 19:32:22 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/03/14 19:40:15 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@
 #define RT_F 2
 
 __attribute__((hot, const, __always_inline__))
-inline t_3dcoordsx8	rt_v8_to_coords(t_v8da v)
+inline t_3dcoordsx8	rt_v8(t_8packd v)
 {
 	return ((t_3dcoordsx8)
 		{
-			.a = {v[0], v[0], v[0], 0},
-			.b = {v[1], v[1], v[1], 0},
-			.c = {v[2], v[2], v[2], 0},
-			.d = {v[3], v[3], v[3], 0},
-			.e = {v[4], v[4], v[4], 0},
-			.f = {v[5], v[5], v[5], 0},
-			.g = {v[6], v[6], v[6], 0},
-			.h = {v[7], v[7], v[7], 0},
+			.a = {v.a, v.a, v.a, 0},
+			.b = {v.b, v.b, v.b, 0},
+			.c = {v.c, v.c, v.c, 0},
+			.d = {v.d, v.d, v.d, 0},
+			.e = {v.x, v.x, v.x, 0},
+			.f = {v.y, v.y, v.y, 0},
+			.g = {v.z, v.z, v.z, 0},
+			.h = {v.w, v.w, v.w, 0},
 		});
 }
 
@@ -57,7 +57,7 @@ inline t_RTPacket	rt_render_packet(t_RTScene *scene, t_RTContext *ctx,
 	t_RTPacket		packet;
 	t_v8da			dx;
 	t_v8da			dy;
-	t_3dcoordsx8	coords[3];
+	t_3dcoordsx8	co[3];
 
 	packet = (t_RTPacket){0};
 	dx = (t_v8da){(double)x, (double)(x + 1), (double)(x + 2),
@@ -69,11 +69,12 @@ inline t_RTPacket	rt_render_packet(t_RTScene *scene, t_RTContext *ctx,
 	dx = (2.0 * (dx + 0.5) * (double)ctx->vp.inv_width - 1.0)
 		* ctx->vp.aspect * ctx->vp.scale;
 	dy = (1.0 - 2.0 * (dy + 0.5) * (double)ctx->vp.inv_height) * ctx->vp.scale;
-	coords[RT_F] = rt_coordbroadcast(ctx->vp.forward);
-	coords[RT_R] = rt_coordbroadcast(ctx->vp.right);
-	coords[RT_U] = rt_coordbroadcast(ctx->vp.up);
-	packet.dir = ft_3dunit8(ft_3dadd8(coords[RT_F],
-				ft_3dadd8(ft_3dmul8(coords[RT_R], rt_v8_to_coords(dx)),
-					ft_3dmul8(coords[RT_U], rt_v8_to_coords(dy)))));
+	co[RT_F] = rt_coordbroadcast(ctx->vp.forward);
+	co[RT_R] = rt_coordbroadcast(ctx->vp.right);
+	co[RT_U] = rt_coordbroadcast(ctx->vp.up);
+	packet.dir = ft_3dunit8(ft_3dadd8(co[RT_F], ft_3dadd8(ft_3dmul8(co[RT_R],
+						rt_v8(*(const t_8packd * restrict const) & dx)),
+					ft_3dmul8(co[RT_U],
+						rt_v8(*(const t_8packd * restrict const) & dy)))));
 	return (packet);
 }
