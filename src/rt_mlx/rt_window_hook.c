9bb16a40591dba6f3cbf44253c552d421b5d8e95
j <castro.jaime@proton.me>
2026-03-18 03:34:15 +0100

    fix: pow2 resize

--- src/rt_mlx/rt_window_hook.c
+++ src/rt_mlx/rt_window_hook.c
@@ -6,13 +6,14 @@
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 02:55:24 by jaicastr          #+#    #+#             */
-/*   Updated: 2026/03/18 03:19:41 by jaicastr         ###   ########.fr       */
+/*   Updated: 2026/03/18 03:33:30 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt_miniRT.h"
 #include "rt_mlx/rt_mlx.h"
 #include "hint.h"
+#define ALERR "Error\nCannot allocate image\n"
 
 __attribute__((__nonnull__(1), hot))
 int	rt_window_hook(t_RTstate *restrict const state)
@@ -27,8 +28,10 @@ int	rt_window_hook(t_RTstate *restrict const state)
 	if (state->ctx.display_height != (size_t)attr.height
 		|| state->ctx.display_width != (size_t)attr.width)
 	{
-		state->ctx.display_height = attr.height;
-		state->ctx.display_width = attr.width;
+		state->ctx.display_height = 1ULL << (64
+				- ft_memclz_u64((size_t)attr.height - 1));
+		state->ctx.display_width = 1ULL << (64
+				- ft_memclz_u64((size_t)attr.width - 1));
 		state->ctx.scene_redraw = 1;
 		state->ctx.rt_img->image->data = NULL;
 		ft_arena_rewind(&state->ctx.rt_arena, state->ctx.rewind_image);
@@ -37,8 +40,7 @@ int	rt_window_hook(t_RTstate *restrict const state)
 			state->ctx.rt_img->pix);
 		state->ctx.rt_img = NULL;
 		if (rt_alloc_imagebuffer(&state->ctx) == KO)
-			(rt_free_state(state), ft_pin_invariant_msg(0,
-					(char *)"Error\nCannot allocate image\n"));
+			(rt_free_state(state), ft_pin_invariant_msg(0, (char *)ALERR));
 	}
 	return (0);
 }
